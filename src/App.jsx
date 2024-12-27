ddcc1372e43eaa7853a2d450f56aa3b1de3338f1
Francis Matthew <matthewabiodun2001@gmail.com>
2024-12-27 08:18:33 +0100

    added a text color or backgorund color

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className='text-green-500'>
+    <div className='text-green-700 bg-white'>
       WELCOME GUYS
     </div>
   )
