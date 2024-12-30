db2542ce2ceda5e04d203cc13bebcf34c156e612
Francis Matthew <matthewabiodun2001@gmail.com>
2024-12-30 03:33:41 +0100

    added the chatbot functionality, favicon and pictures to the service pages

--- src/pages/ServiceUsPage.jsx
+++ src/pages/ServiceUsPage.jsx
@@ -1,4 +1,7 @@
 // import React from 'react'
+import Service1 from '../assets/service1.jpg';
+import Service2 from '../assets/service2.jpg';
+import Service3 from '../assets/service3.jpg';
 
 const ServiceUsPage = () => {
   return (
@@ -8,17 +11,17 @@ const ServiceUsPage = () => {
         <h2 className="text-3xl font-bold text-blue-600 mb-8">Our Services</h2>
         <div className="flex flex-wrap justify-center gap-8">
           <div className="service-item bg-white p-8 rounded-lg shadow-md transition-transform duration-300 hover:transform hover:translate-y-4">
-            <img src="./assets/profile pic-png.png" alt="Service 1" className="w-32 h-32 mb-6 mx-auto" />
+            <img src={Service1} alt="Service 1" className=" mb-6 mx-auto " />
             <h3 className="text-2xl font-semibold mb-4">Service One</h3>
-            <p className="text-lg text-gray-600">Here in HELIX, we have the best visual brand designers.</p>
+            <p className="text-lg text-gray-600">Here in Phantom, we have the best visual brand designers.</p>
           </div>
           <div className="service-item bg-white p-8 rounded-lg shadow-md transition-transform duration-300 hover:transform hover:translate-y-4">
-            <img src="./assets/WhatsApp Image 2024-12-27 at 01.26.45_ceb677fb.jpg" alt="Service 2" className="w-32 h-32 mb-6 mx-auto" />
+            <img src={Service2} alt="Service 2" className=" mb-6 mx-auto" />
             <h3 className="text-2xl font-semibold mb-4">Service Two</h3>
             <p className="text-lg text-gray-600">Description of the second service offered by Phantom.</p>
           </div>
           <div className="service-item bg-white p-8 rounded-lg shadow-md transition-transform duration-300 hover:transform hover:translate-y-4">
-            <img src="./assets/WhatsApp Image 2024-12-27 at 01.26.46_552352b3.jpg" alt="Service 3" className="w-32 h-32 mb-6 mx-auto" />
+            <img src={Service3} alt="Service 3" className=" mb-6 mx-auto" />
             <h3 className="text-2xl font-semibold mb-4">Service Three</h3>
             <p className="text-lg text-gray-600">Description of the third service offered by Phantom.</p>
           </div>
